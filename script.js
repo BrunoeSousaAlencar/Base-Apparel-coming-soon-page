7b7015f0dd641fc4773eb3caadf296e434937118
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-08-23 16:21:41 -0300

    em andamento...

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("#form")
+const email = document.querySelector('#email').value;
+
+document.querySelector('#email').focus();
+
+form.addEventListener("submit", (e)=>{
+    
+})
+
+console.log(document.querySelector('#email').value)
