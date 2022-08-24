d3818ae48c88dbb9cd08303108da7b7b9ba2941d
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-08-24 14:18:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const email = document.querySelector('#email').value;
 document.querySelector('#email').focus();
 
 form.addEventListener("submit", (e)=>{
-    
+    e.preventDefault();
+    if(email === ""){
+        document.querySelector(".erro").textContent =''
+    }else{
+        document.querySelector(".erro").textContent ='erro'
+        e.preventDefault();
+    }
 })
 
 console.log(document.querySelector('#email').value)
