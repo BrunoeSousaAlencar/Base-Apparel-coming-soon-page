4ea5add5cfe7c2cee3bb15ff54001fab1bc82211
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-08-24 16:38:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ document.querySelector('#email').focus();
             if(email ==""){
                 document.querySelector("#erro").textContent = "pleace provide a valid email"; 
                 document.querySelector('#img-erro').style.display = "block"
+                document.querySelector('#email').style.border = "1.5px solid var(--Soft-Red)";
                 e.preventDefault();
+
             } else{
                 document.querySelector("#erro").textContent = "";
                 document.querySelector('#img-erro').style.display = "none"
+                document.querySelector('#email').style.border = "1px solid var(--color2)";
             }
         })
         console.log(email)
