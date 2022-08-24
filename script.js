3bb0b37bb8922720fd57b2dc678f7e75e8e5338e
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-08-24 16:21:06 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const form = document.querySelector("#form")
-const email = document.querySelector('#email').value;
-
 document.querySelector('#email').focus();
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    if(email === ""){
-        document.querySelector(".erro").textContent =''
-    }else{
-        document.querySelector(".erro").textContent ='erro'
-        e.preventDefault();
-    }
-})
-
-console.log(document.querySelector('#email').value)
+        form.addEventListener("submit", (e)=>{
+            var form = document.querySelector('#form');
+            var email = document.querySelector('#email').value;
+            var erro = document.querySelector('#erro');
+            if(email ==""){
+                document.querySelector("#erro").textContent = "pleace provide a valid email"; 
+                document.querySelector('#img-erro').style.display = "block"
+                e.preventDefault();
+            } else{
+                document.querySelector("#erro").textContent = "";
+                document.querySelector('#img-erro').style.display = "none"
+            }
+        })
+        console.log(email)
